5547d01b2ce86b35b43944d9542ba4846c1d819b
PureWasHere <Codystevenson123@gmail.com>
2020-03-17 11:39:42 -0700

    Built OAuth Challenge Block

--- PureLib/OAuthChallenge.cs
+++ PureLib/OAuthChallenge.cs
@@ -4,20 +4,24 @@ using RuriLib;
 using RuriLib.LS;
 using System;
 using System.Security.Cryptography;
+using System.Text;
 
 namespace OpenBulletPlugin
 {
-    public class Verifier : BlockBase, IBlockPlugin
+    public class Challenge : BlockBase, IBlockPlugin
     {
-        public string Name => "OAuthVerifier";
-        public string Color => "Aqua";
+        public string Name => "OAuthChallenge";
+        public string Color => "Blue";
         public bool LightForeground => false;
         [Text("Variable Name", "The output variable name")]
         public string VariableName { get; set; } = "";
 
+        [Text("Verifier", "The Result from Verifier Gen")]
+        public string VerInput { get; set; } = "";
+
         [Checkbox("Is Capture", "Should the output variable be marked as capture?")]
         public bool IsCapture { get; set; } = false;
-        public Verifier()
+        public Challenge()
         {
             Label = Name;
         }
@@ -26,6 +30,8 @@ namespace OpenBulletPlugin
             var input = line.Trim();
             if (input.StartsWith("#")) // If the input actually has a label
                 Label = LineParser.ParseLabel(ref input); // Parse the label and remove it from the original string
+            VerInput = LineParser.ParseLiteral(ref input, "First Number");
+
             if (LineParser.ParseToken(ref input, TokenType.Arrow, false) == "")
                 return this;
             try
@@ -43,7 +49,8 @@ namespace OpenBulletPlugin
         {
             var writer = new BlockWriter(GetType(), indent, Disabled)
                 .Label(Label) // Write the label. If the label is the default one, nothing is written.
-                .Token(Name); // Write the block name. This cannot be changed.
+                .Token(Name) // Write the block name. This cannot be changed.
+                .Literal(VerInput);
             if (!writer.CheckDefault(VariableName, nameof(VariableName)))
             {
                 writer
@@ -55,16 +62,20 @@ namespace OpenBulletPlugin
         }
         public override void Process(BotData data)
         {
-            base.Process(data);
-            byte[] number = new byte[32];
-            RandomNumberGenerator rng = RandomNumberGenerator.Create();
-            rng.GetBytes(number);
-            var bytes = (BitConverter.ToString(number));
-            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(bytes);
+            var Input = (ReplaceValues(VerInput, data));
+            byte[] bytes = Encoding.UTF8.GetBytes(Input);
+            SHA256Managed hashstring = new SHA256Managed();
+            byte[] hash = hashstring.ComputeHash(bytes);
+            string hashString = Input;
+            foreach (byte x in hash)
+            {
+                hashString += String.Format("{0:x2}", x);
+            }
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(hashString);
             string encodedStr = System.Convert.ToBase64String(plainTextBytes);
             var result = encodedStr;
             InsertVariable(data, IsCapture, result, VariableName, "", "", false, false);
-            data.Log($"Generated OAuth Verifier with result {result}");
+            data.Log($"Generated OAuth Challenge with result {result}");
         }
     }
 }
--- PureLib/OCR.cs
+++ PureLib/OCR.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Anomaly
+﻿namespace Anomaly
 {
-    class OCR
+    internal class OCR
     {
     }
 }
