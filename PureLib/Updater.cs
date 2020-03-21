a3bf996a667f7afbf32c3c381258209fae850223
PureWasHere <Codystevenson123@gmail.com>
2020-03-21 03:53:38 -0700

    Adding Changelog test 1

--- PureLib/Updater.cs
+++ PureLib/Updater.cs
@@ -2,11 +2,8 @@
 using PluginFramework.Attributes;
 using RuriLib;
 using RuriLib.Interfaces;
-using RuriLib.Models;
-using RuriLib.ViewModels;
-using System.IO;
+using Extreme.Net;
 using System.Net.Http;
-using System.Windows;
 
 namespace Anomaly
 {
@@ -14,12 +11,15 @@ namespace Anomaly
     {
         public string Name => "Anomaly Updater";
         public string UpdateStatus { get; set; } = "...";
-        private static readonly HttpClient client = new HttpClient();
         [Button("Check For Update")]
         public async System.Threading.Tasks.Task ExecuteAsync(IApplication app)
         {
             string VersionURL = "https://raw.githubusercontent.com/PurityWasHere/OpenBullet-Anomaly-Plugin/master/VersionNumber.txt";
+            string ChangelogURL = "https://raw.githubusercontent.com/PurityWasHere/OpenBullet-Anomaly-Plugin/master/Changelog.txt";
             string LocalVersion = "0.01";
+            var client = new HttpClient();
+            var request = new HttpRequestMessage();
+            request.Method = System.Net.Http.HttpMethod.Get;
             var response = await client.GetAsync(VersionURL);
             var responseString = await response.Content.ReadAsStringAsync();
             if (responseString.Trim().Contains(LocalVersion))
@@ -35,5 +35,8 @@ namespace Anomaly
                 app.Logger.Log("Error Checking For Update.", LogLevel.Info, true);
             }
         }
+        [TextMulti("Changelog", "Full of Yummies")]
+        public string[] Interests { get; set; } = new string[] {  };
+    };
     }
 }
