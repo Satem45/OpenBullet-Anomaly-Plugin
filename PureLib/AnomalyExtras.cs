3742af6d32ecd85cfa20e5f5650dfd71f7db7b06
PureWasHere <Codystevenson123@gmail.com>
2020-03-24 07:26:40 -0700

    Added Extension renamer for Converting All .Anom Configs to .Loli

---
+++ PureLib/AnomalyExtras.cs
@@ -0,0 +1,33 @@
+﻿using PluginFramework;
+using PluginFramework.Attributes;
+using RuriLib;
+using RuriLib.Interfaces;
+using System;
+using System.IO;
+
+namespace Anomaly
+{
+    public class AnomalyExtras : IPlugin
+    {
+        public string Name => "Anomaly Extras";
+
+        [Button("Convert .Anom to .Loli")]
+        public void Convert(IApplication app)
+        {
+            try
+            {
+                string[] Files = Directory.GetFiles(@".\Configs");
+                foreach (string CurFile in Files)
+                {
+                    if (CurFile.Contains(".anom"))
+                    {
+                        string NewName = Path.ChangeExtension(CurFile, ".loli");
+                        File.Move(CurFile, NewName);
+                    }
+                }
+                app.Logger.Log($"renamed Config Extensions", LogLevel.Info, false);
+            }
+            catch (Exception ex) { app.Logger.Log($"An Error Occured While trying to rename files: {ex}", LogLevel.Error, true); }
+        }
+    }
+}
